5920f903cbfe070603e1b3d7c9307ba6d168382f
songwenhui <songwen1073@126.com>
2021-09-07 11:20:19 +0800

    ceshi

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,27 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import Taro, { useDidShow } from '@tarojs/taro';
 import { View, Map, Text } from '@tarojs/components'
 
 import './index.scss'
 
 const Index = () => {
 
+  const [mapCtx, setMapCtx] = useState<Taro.MapContext>();
+  const [center, setCenter] = useState({ lat: 0, lng: 0 });
+
+  useEffect(() => {
+  })
+
+  useDidShow(() => {
+    if (Taro.getEnv() !== Taro.ENV_TYPE.WEB) {
+      const ctx = Taro.createMapContext('map') as Taro.MapContext;
+      setMapCtx(ctx);
+    }
+    setTimeout(() => {
+      const _center = {lng: 116.35297035953413, lat: 40.014460424314755};
+      console.log('_center ---> ', _center);
+      setCenter(_center);
+    }, 5000);
+  });
+
   const regionChange = (e) => {
     console.log('e ---> ', e);
+    if (e.type === 'end') {
+      mapCtx?.getCenterLocation({
+        success: (data) => {
+          const { longitude, latitude } = data;
+          console.log('longitude, latitude ---> ', longitude, latitude);
+        },
+        fail: (err) => {
+          console.log('err ---> ', err);
+        },
+      });
+    }
   }
 
   return (
     <View className='index'>
       <Map
         id='map'
-        latitude={40.030609}
-        longitude={116.355688}
+        latitude={center?.lat || 40.030609}
+        longitude={center?.lng || 116.355688}
         enable-scroll
         scale={12}
         onRegionChange={regionChange}
-        // @ts-ignore
-        bindRegionChange={regionChange}
-        // @ts-ignore
-        onBegin={regionChange}
-        onEnd={regionChange}
       ></Map>
     </View>
   )
